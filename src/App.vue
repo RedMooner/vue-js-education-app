59b8292d9c49bd24f78ee5c68ff682d142c12b52
RedMoon <SashaM122@mail.ru>
2023-03-22 10:44:26 +0500

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div class="app">
+      <post-form @create='createPost'></post-form>
+      <post-list :posts="posts">
+      </post-list>
+    </div>
+
+  </div>
 </template>
+<script>
+import PostForm from '@/components/PostForm.vue'
+import PostList from '@/components/PostList.vue'
+export default {
+  components: {
+    PostForm, PostList
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'js1', body: 'Описание1' },
+        { id: 2, title: 'js2', body: 'Описание2' },
+        { id: 3, title: 'js3', body: 'Описание3' },
+      ],
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+      console.log('tst')
+    },
+
+  }
+}
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  padding: 20px;
 }
 </style>
